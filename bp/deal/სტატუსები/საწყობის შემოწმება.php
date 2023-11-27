f9e6913e89444e134454dbfc9b2bd337af06f7b7
Tornikee8 <nightwar8@gmial.com>
2023-11-27 19:01:08 +0400

    123

--- bp/deal/სტატუსები/საწყობის შემოწმება.php
+++ bp/deal/სტატუსები/საწყობის შემოწმება.php
@@ -42,60 +42,7 @@ foreach($prods as $prod){
     $Product=getCIBlockElementsByFilter_7S($arFilter);
     $categoryID=$Product[0]["IBLOCK_SECTION_ID"];
     
-    if($categoryID == "16" || $categoryID == "18" || <?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("test");
-
-function printArr($arr) {
-    echo "<pre>"; print_r($arr); echo "</pre>";
-}
-
-
-function getCIBlockElementsByFilter_7S($arFilter = array(),$sort = array()){
-    $arElements = array();
-    $arSelect = array("ID", "IBLOCK_SECTION_ID", "NAME", "DATE_ACTIVE_FROM");
-    $res = CIBlockElement::GetList($sort, $arFilter, false, array("nPageSize" => 50), $arSelect);
-    while ($ob = $res->GetNextElement()) {
-        $arFilds = $ob->GetFields();
-        $arProps = $ob->GetProperties();
-        $arPushs = array();
-        foreach ($arFilds as $key => $arFild) $arPushs[$key] = $arFild;
-        foreach ($arProps as $key => $arProp) $arPushs[$key] = $arProp["VALUE"];
-        array_push($arElements, $arPushs);
-    }
-    return $arElements;
-}
-
-
-
-
-$root=$this->GetRootActivity();
-$deal_ID=$root->GetVariable("deal_ID");
-
-// $deal_ID=36;
-
-
-$canSell = "NO";
-
-$prods = CCrmDeal::LoadProductRows($deal_ID);
-
-foreach($prods as $prod){
-
-
-    $arFilter=array("ID"=>$prod['PRODUCT_ID']);
-    $Product=getCIBlockElementsByFilter_7S($arFilter);
-    $categoryID=$Product[0]["IBLOCK_SECTION_ID"];
-    
     if($categoryID == "16" || $categoryID == "18" || $categoryID == "19"){
-        $canSell = "YES";
-    }else{
-        $canSell = "NO";
-    }
-    
-}
-
-$this->SetVariable("canSell" , $canSell);
-){
         $canSell = "YES";
     }else{
         $canSell = "NO";
